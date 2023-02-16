fc54712c9a4a65a4b45bdc7d84eb4b11d00260aa
Biekro Godbless <biekrogodbless@gmail.com>
2023-02-16 08:17:28 -0800

    puts function to display comtent in the terminal

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
+/* Function prints strings*/
 int main(void)
 {
-	puts(" Programming is like building a multilingual puzzle \n ");
+	puts("\"Programming is like building a multilingual puzzle \n ");
 return (0);
 }
