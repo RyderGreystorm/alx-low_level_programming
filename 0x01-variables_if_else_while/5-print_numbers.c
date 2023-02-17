a14a9db577cd28cd663ff73b55074a7396f76b5a
Biekro Godbless <biekrogodbless@gmail.com>
2023-02-17 05:55:03 -0800

    Base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry level of our application
+ * Return: returns 0 for success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
