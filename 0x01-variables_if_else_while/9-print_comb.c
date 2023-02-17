3b1718373c93e2ae1984f8a101947b6d2e6c46f7
Biekro Godbless <biekrogodbless@gmail.com>
2023-02-17 11:14:37 -0800

    combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - woking loops
+ * Return: zero for success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		if (i == 9)
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
