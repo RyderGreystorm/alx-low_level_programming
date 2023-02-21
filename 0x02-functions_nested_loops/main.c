e563769f2436df463c122dd3725a7c2b11523a8c
Biekro Godbless <biekrogodbless@gmail.com>
2023-02-21 08:41:09 -0800

    checking if alphabet is lower

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -7,7 +7,14 @@
 
 int main(void)
 {
+	int r;
 
-	print_alphabet_x10();
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
 	return 0;
 }
