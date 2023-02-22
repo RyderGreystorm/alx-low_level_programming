bc0ed8a3e446c21a14a7f8bb4336a51419095ef7
Biekro Godbless <biekrogodbless@gmail.com>
2023-02-21 17:21:33 -0800

    printing 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,22 +12,24 @@ void print_to_98(int n)
 	{
 		for (n = n; n <= 98; n++)
 		{
-			_putchar(n + '0');
+			printf("%d", n);
 			if (n == 98)
 				break;
-			_putchar(',');
-			_putchar(' ');
+			printf(",");
+			printf(" ");
 		}
+		printf("\n");
 	}
 	else
 	{
 		for (n = n; n >= 98; n--)
 		{
-			_putchar(n + '0');
+			printf("%d", n);
 			if (n == 98)
 				break;
-			_putchar(',');
-			_putchar(' ');
+			printf(",");
+			printf(" ");
 		}
+		printf("\n");
 	}
 }
