796ff45f3d989cd827df3e4ce5663d50fb87d545
Biekro Godbless <biekrogodbless@gmail.com>
2023-02-22 14:09:47 -0800

    main.c main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry level into the application
+ * Return: 0 for success
+ */
+
+int main(void)5
+{
+	int i = 98;
+
+	 positive_or_negative(i);
+
+	return (0);
+}
