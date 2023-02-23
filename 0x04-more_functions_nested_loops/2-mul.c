a5b7855fbbd1688338a85af2367b0963bba8daf0
Biekro Godbless <biekrogodbless@gmail.com>
2023-02-23 12:23:52 -0800

    multiplying two values

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two values passed as arguments
+ * @a: first parameter
+ * @b: second parameter
+ * Return: result as type int
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
