196edbddd41a72f17da1c24b8bd51426352c2635
Biekro Godbless <biekrogodbless@gmail.com>
2023-02-23 16:37:32 -0800

    fb

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i == 100)
-			printf("Buzz ");
+			printf("%d ", i);
 		else if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else if (i % 3	== 0)
