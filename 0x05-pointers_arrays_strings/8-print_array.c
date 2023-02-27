6513849270849f007c6d1b36316452c9b39ffe48
Biekro Godbless <biekrogodbless@gmail.com>
2023-02-27 14:22:18 -0800

    print arr

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - prints an array
+ * @a: array parameter
+ * @n: array length
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d, ", a[i]);
+		if (i == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
+	}
+	printf("\n");
+}
