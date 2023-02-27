a0cd1c35bf12b8c756138d08f1007688c39585d1
Biekro Godbless <biekrogodbless@gmail.com>
2023-02-27 13:20:13 -0800

    every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - prints the first and very other charater
+ * @str: parameter
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i, len;
+
+	len = strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		if (i % 2 == 1)
+			continue;
+		else
+			_putchar(str[i]);
+	}
+}
