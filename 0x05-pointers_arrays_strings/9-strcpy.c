256fe461d5add8621930b6addcbc125b400261af
Biekro Godbless <biekrogodbless@gmail.com>
2023-02-27 14:48:42 -0800

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *char *_strcpy - copy a string to another
+ * @dest: destination string of the function
+ * @src: string from source getting appended to other string
+ *Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
