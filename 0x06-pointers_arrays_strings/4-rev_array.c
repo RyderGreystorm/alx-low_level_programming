82dbf4a7903ea688e75075a4fb2d893017f8dca6
Biekro Godbless <biekrogodbless@gmail.com>
2023-03-01 08:27:42 -0800

    reversing an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - sorts arrays from highest value
+ *a@: array
+ *@n: lenght he array
+ *Return:arrrray
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, x;
+
+	for (i = 0; i < (n / 2); i++)
+	{
+		x = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = x;
+	}
+}
