d2d4ffe1946e569309281c0d37967d758e58db50
Biekro Godbless <biekrogodbless@gmail.com>
2023-03-01 11:30:10 -0800

    updated debugging for strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,13 +10,6 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int len, i;
-
-	len = strlen(dest);
-
-	for (i = 0; i < n; i++)
-	{
-		dest[len + i] = src[i];
-	}
+	strncat(dest, src, n);
 	return (dest);
 }
