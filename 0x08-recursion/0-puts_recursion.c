dc376abb5c310232919ed8c2df7733eea7ca5b40
Biekro Godbless <biekrogodbless@gmail.com>
2023-03-08 11:02:59 -0800

    print string using puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+/**
+ * _puts_recursion - uses the puts function to display output
+ * @s: string passed as a pointer parameter
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
