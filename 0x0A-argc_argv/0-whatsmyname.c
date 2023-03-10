f5e052e3064a69613b7ffe2c19140044e8a323b3
Biekro Godbless <biekrogodbless@gmail.com>
2023-03-10 14:25:52 -0800

    argv[0] making reference to name of file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - entry level of the application
+ * @argc: params that counts the number of inputs passed on the command line
+ * @argv: pointer string
+ * Return: 0 for success
+ */
+
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This project explores argv and argc
