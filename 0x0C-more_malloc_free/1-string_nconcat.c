d4121b5f33e51123ad832d1957494ee6a71bf362
Biekro Godbless <biekrogodbless@gmail.com>
2023-03-16 10:27:13 -0700

    hot fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,8 +14,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	char *ptr;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	len = strlen(s1);
 
 	ptr = malloc(sizeof(char) * (len + n + 1));
