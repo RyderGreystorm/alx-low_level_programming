2a68c04a2e1af4782ffc78e776af2c6430c30949
Biekro Godbless <biekrogodbless@gmail.com>
2023-03-20 15:31:13 -0700

    using __FILE__

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main entry level of programm
+ * @void: carries no params
+ * Return: zero always
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
