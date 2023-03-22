adaaf011259d1e2973084ed6bc6c49d78ed81727
Biekro Godbless <biekrogodbless@gmail.com>
2023-03-22 05:49:07 -0700

    This is my first commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints the name passed as argument
+ * @name: string passed as argument
+ * @f: pointer to a function
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+This project is about function pointers and thier callbacks. 
+In ths project I will complete tasks on function pointers.
+This is part of my low level programming course
