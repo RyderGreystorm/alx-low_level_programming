5e81bc8a59e5bfc8d8be2807a3ed4185d5916463
Biekro Godbless <biekrogodbless@gmail.com>
2023-03-30 06:50:39 -0700

    changing the printing algorithm

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,13 +12,12 @@ size_t print_list(const list_t *h)
 
 	count = 0;
 
-	if (h->str == NULL)
-		printf("[0] (nil)\n");
-
 	while (h != NULL)
 	{
-		if (h->str)
-			printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s \n",h->len, h->str);
 		count++;
 		h = h->next;
 	}
