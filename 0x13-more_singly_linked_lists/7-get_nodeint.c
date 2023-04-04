d787a8a8a45f90bdaa1a18bb684fd2a4ccee2956
Biekro Godbless <biekrogodbless@gmail.com>
2023-04-04 08:21:48 -0700

    find node at nth position

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - find node in a particular index
+ * @head: address of the head pointer passed to the function
+ * @index:position  at which node islocated
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *tmp;
+	unsigned int n = index;
+
+	tmp = head;
+
+	while(n != 0)
+	{
+		tmp = tmp->next;
+		n--;
+	}
+	if (tmp == NULL)
+		return NULL;
+	else
+		return (tmp);
+	
+}
