ae0a43ec8df5cdfb8c2a36e196b3bd9f60319395
Biekro Godbless <biekrogodbless@gmail.com>
2023-04-04 12:30:18 -0700

    hot fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	idx--;
 
-	while (idx != 0 && ptr)
+	while (idx != 0 && ptr != NULL)
 	{
 		ptr = ptr->next;
 		idx--;
