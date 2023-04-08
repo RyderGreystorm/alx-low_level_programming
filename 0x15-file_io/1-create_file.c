27ac85e147b65d552a916d749a40324cfa770738
Biekro Godbless <biekrogodbless@gmail.com>
2023-04-08 15:59:14 -0700

    checked if text content is null

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,11 +9,13 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wr, len = strlen(text_content);
+	int fd, wr, len;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT| O_RDWR| O_TRUNC, 0600);
+	if (text_content != NULL)
+		len = strlen(text_content);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(fd, text_content, len);
 
 	if (fd == -1 || wr == -1)
