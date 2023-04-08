766a238bb0cd802b931665cf204a4db393bbc009
Biekro Godbless <biekrogodbless@gmail.com>
2023-04-08 15:53:36 -0700

    create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and writes into the file
+ * @filename: pointer to the buffer where chars is stored
+ * @text_content: string
+ * Return: 1 on success and -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr, len = strlen(text_content);
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT| O_RDWR| O_TRUNC, 0600);
+	wr = write(fd, text_content, len);
+
+	if (fd == -1 || wr == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
