d4e8bbdcd838f764d52983578495aece6c172876
Biekro Godbless <biekrogodbless@gmail.com>
2023-04-09 04:08:58 -0700

    hot fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,12 +9,15 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wr, len = strlen(text_content);
+	int fd, wr, len;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		text_content = "";
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(fd, text_content, len);
 
