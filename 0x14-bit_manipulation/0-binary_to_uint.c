f28ba7ddb01b4deb1039e15b905a5dc15707ed8e
Biekro Godbless <biekrogodbless@gmail.com>
2023-04-10 08:03:20 -0700

    fixing errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,7 +8,7 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, expo = 1, rem, bin, i = 0;
+	unsigned int num = 0, i = 0;
 
 	if (b == NULL)
 		return (0);
@@ -17,17 +17,8 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < 48 || b[i] > 49)
 			return (0);
+		num = 2 * num + (b[i] - '0');
 		i++;
-	}
-
-	bin = atoi(b);
-
-	while (bin != 0)
-	{
-		rem = bin % 10;
-		num = num + (expo * rem);
-		expo = expo * 2;
-		bin = bin / 10;
 	}
 	return (num);
 }
