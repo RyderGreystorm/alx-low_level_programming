50eaaddb899b0a675d91f6392b67e6904c63f0ff
Biekro Godbless <biekrogodbless@gmail.com>
2023-04-10 14:21:48 -0700

    little or big endiann

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * @void: void
+ * Return:0  for big, 1 for little;
+ */
+
+int get_endianness(void)
+{
+	unsigned int low = 1;
+
+	char *ptr = (char *) &low;
+	if (*ptr)
+		return (1);
+	else
+		return (0);
+}
