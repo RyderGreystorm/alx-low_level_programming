5ec19684fa19e7a306b0f5501a595cb63884bfca
Biekro Godbless <biekrogodbless@gmail.com>
2023-04-10 13:48:14 -0700

    hot fic

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,7 +4,7 @@
  * set_bit - sets the value of a bit to 1 at a given index.
  * @n: number being passed for operation
  * @index: index of operation
- * Return 1 for success and -1 on failure
+ * Return: 1 for success and -1 on failure
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
