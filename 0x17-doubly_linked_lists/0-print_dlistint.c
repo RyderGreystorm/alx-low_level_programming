8f6b8311fa6e53048c91dd6f564e6cc12b9b2ed3
Godbless Biekro <biekrogodbless@gmail.com>
2023-05-18 15:02:54 -0700

    print data and return sum of list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints the elements in the node
+ * @h: head pointer
+ * Return: number of nodes in the list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t sum = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		sum += 1;
+	}
+	printf("\n");
+	return (sum);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,4 @@
+# DOUBLY LINKED LISTS
+
+This project is based on doubly linked lists in data structures. Here we are using the C programming language
+to solve linked list tasks.
