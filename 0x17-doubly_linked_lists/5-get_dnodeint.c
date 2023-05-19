1305f45347f51d25d9b5a7f969b4542ccf861b73
Godbless Biekro <biekrogodbless@gmail.com>
2023-05-18 17:54:35 -0700

    fetch node at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - get node at a particular index
+ * @head: head pointer
+ * @index: intex of interest
+ * Return: node at the index of interest
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *ptr = head;
+
+	while (index > 0)
+	{
+		ptr = ptr->next;
+		index--;
+	}
+	if (!ptr)
+		return (NULL);
+	return (ptr);
+}
