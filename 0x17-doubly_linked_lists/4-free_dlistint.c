89b79c2bb7adef648d1bcc5b007b444ba92f5018
Godbless Biekro <biekrogodbless@gmail.com>
2023-05-18 17:26:26 -0700

    memory leak fixed

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees memories allocaated to list
+ * @head: head pointer
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current, *next;
+
+	current = head;
+
+	while (current->next != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
