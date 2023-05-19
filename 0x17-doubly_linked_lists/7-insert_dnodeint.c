13df0784c3d607dfc85dc41b1189d309ad514b84
Godbless Biekro <biekrogodbless@gmail.com>
2023-05-18 18:47:58 -0700

    fetch node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,37 +8,46 @@
  * Return: new node
  */
 
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *current;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	dlistint_t *current = *h;
+
 	unsigned int count = 0;
 
 	if (h == NULL)
 		return (NULL);
-
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	current = *h;
+	if (new_node == NULL)
+	return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
+	if (*h == NULL)
+	{
+		*h = new_node;
+		return (new_node);
+	}
 	while (current != NULL && count < idx - 1)
 	{
 		current = current->next;
 		count++;
 	}
 
-	if (current == NULL || current->next == NULL)
+	if (current == NULL)
+	{
+		free(new_node);
 		return (NULL);
-
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->prev = current;
+	}
 	new_node->next = current->next;
+	new_node->prev = current;
+	if (current->next != NULL)
 	current->next->prev = new_node;
 	current->next = new_node;
-
 	return (new_node);
 }
