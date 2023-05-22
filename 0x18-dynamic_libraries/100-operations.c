abe5e402c098cf50e28ba57d0821fbe4984765a7
Godbless Biekro <biekrogodbless@gmail.com>
2023-05-22 16:19:55 -0700

    python and c codes

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -15,7 +15,7 @@ int mul(int a, int b)
 	return (a * b);
 }
 
-int divi(int a, int b)
+int div(int a, int b)
 {
 	return (a / b);
 }
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -4,7 +4,6 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
